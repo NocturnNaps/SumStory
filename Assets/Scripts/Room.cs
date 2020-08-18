f3003cdaa8de8e1ea4b04f1efcb5f2e8e98642f0
Nocturn Naps <48245084+NocturnNaps@users.noreply.github.com>
2020-08-18 04:35:31 +0300

    Rooms

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int NumberOfRooms;
+    [Header("Doors")]
+    public GameObject RightDoor;
+    public GameObject LeftDoor;
+    public GameObject UpperDoor;
+    public GameObject DownDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
